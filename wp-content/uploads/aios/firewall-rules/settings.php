3380efd66bc02c4a5b42dfb978c95170807a71d5
Pece Mitrevski <peckomitrevski@gmail.com>
2022-11-23 15:13:08 +0100

    First commit

---
+++ wp-content/uploads/aios/firewall-rules/settings.php
@@ -0,0 +1,6 @@
+<?php __halt_compiler();
+/**
+ * This file was created by All In One Security (AIOS) plugin.
+ * The file is required for storing and retrieving your firewall's settings.
+ */
+{"aios_enable_rename_login_page":"","aios_login_page_slug":"","aios_enable_brute_force_attack_prevention":"","aios_brute_force_secret_word":"","aios_cookie_based_brute_force_redirect_url":"http:\/\/127.0.0.1","aios_brute_force_attack_prevention_pw_protected_exception":"","aios_brute_force_attack_prevention_ajax_exception":"","aios_brute_force_secret_cookie_name":"aios_brute_force_secret_2fa2b0ecb568acb3c157c29b34db396e","aios_brute_force_cookie_salt":":2,gr3zA,EhTfj$1d)+zcgKx_MsFU+b(^t:s@UaYA`M.&aFC2G7&uJK8S\/`P4]dO+UE)mA>HW [,]XN8K3:_:3yiYYv%o4WBu-.*Em)a=1D#m.C&~v_IT>*suac>76|*"}
